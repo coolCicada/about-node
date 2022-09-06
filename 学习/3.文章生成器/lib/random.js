92c035a319027824a9c141fad56d2cdf8914ccea
liangsai <liangsai@meituan.com>
2022-09-06 14:53:57 +0800

    feat:随机库

---
+++ 学习/3.文章生成器/lib/random.js
@@ -0,0 +1,16 @@
+export function randomInt(min, max) {
+  const p = Math.random();
+  return Math.floor(min + (max - min) * p);
+}
+
+export function createRandomPicker(arr) {
+  arr = [...arr];
+  function randomPick() {
+    const len = arr.length - 1;
+    const index = randomInt(0, len);
+    [arr[index], arr[len]] = [arr[len], arr[index]];
+    return arr[index];
+  }
+  randomPick();
+  return randomPick;
+}
