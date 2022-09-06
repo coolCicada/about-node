d3b22b2e0266c68219dae5b697164494a6f20950
唐郎不馋 <1475395755@qq.com>
2022-09-06 15:51:53 +0800

    feat:完成生成句子

--- 学习/3.文章生成器/index.js
+++ 学习/3.文章生成器/index.js
@@ -1,12 +1,48 @@
 import { readFile } from 'fs';
 import { fileURLToPath } from 'url';
 import { dirname, resolve } from 'path';
+import { createRandomPicker, sentence } from './lib/random.js';
 
 const url = import.meta.url;
 const path = resolve(dirname(fileURLToPath(url)), 'corpus/data.json');
 readFile(path, (err, data) => {
   if (!err) {
-    console.log(data.toString('utf-8'));
+    const jsonStr = data.toString('utf-8');
+    getRes(JSON.parse(jsonStr));
   }
   else console.error(err);
 });
+
+function getRes(corpus){
+  const {
+    famous,
+    bosh_before,
+    bosh,
+    said,
+    conclude,
+    title,
+  } = corpus;
+
+  const [
+      pickFamous,
+      pickBoshBefore,
+      pickBosh,
+      pickSaid,
+      pickConclude,
+      pickTitle
+  ] = [
+    famous,
+    bosh_before,
+    bosh,
+    said,
+    conclude,
+    title
+  ].map((item) => {
+    return createRandomPicker(item);
+  });
+
+  const r = sentence(pickFamous, { said: pickSaid, conclude: pickConclude });
+  const r2 = sentence(pickBosh, { title });
+  console.log(r);
+  console.log(r2);
+}
--- 学习/3.文章生成器/lib/random.js
+++ 学习/3.文章生成器/lib/random.js
@@ -8,9 +8,22 @@ export function createRandomPicker(arr) {
   function randomPick() {
     const len = arr.length - 1;
     const index = randomInt(0, len);
+    const picked = arr[index];
     [arr[index], arr[len]] = [arr[len], arr[index]];
-    return arr[index];
+    return picked;
   }
   randomPick();
   return randomPick;
+}
+
+export function sentence(pick, replacer) {
+  let ret = pick();
+  for (const key in replacer) {
+    ret = ret.replace(new RegExp(`{{${key}}}`, 'g'),
+      typeof replacer[key] === 'function'
+        ? replacer[key]()
+        : replacer[key]
+      );
+  }
+  return ret;
 }
