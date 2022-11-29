d6c21c8ce18ffc69082c038a38b4da28abaac8ff
coolCicada <1475395755@qq.com>
2022-11-29 21:28:49 +0800

    feat:http模块

--- 学习/4.网络/http-simple.js
+++ 学习/4.网络/http-simple.js
@@ -1,8 +1,15 @@
 import http from 'http';
-import url from 'url';
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  const pathname = req.url;
+
+  if (pathname === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('<h1>Hello world</h1>');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.end('<h1>Not Found</h1>');
+  }
 });
 
 server.listen(8080, () => {
